7629c8658a6eb430c6be533dc612fe1f8965fc6e
Alexandre <akalend@mail.ru>
2024-03-04 14:23:03 +0300

    Update debug.c

--- debug.c
+++ debug.c
@@ -5,8 +5,6 @@
 #include "fmgr.h"
 #include "c.h"
 
-#include "access/global_csn_log.h"
-#include "access/slru.h"
 #include "utils/fmgrprotos.h"
 
 PG_MODULE_MAGIC;
@@ -14,18 +12,12 @@ PG_MODULE_MAGIC;
 
 PG_FUNCTION_INFO_V1(debug_1);
 
+/* the tamplate function for debuging */
 Datum
 debug_1(PG_FUNCTION_ARGS)
 {
-
-	// SlruCtl slru = exportGlobalCSNData();
-
-	TransactionId xid = (TransactionId) DirectFunctionCall1(pg_current_xact_id, -1); 
+	TransactionId xid = 0; 
 	elog(WARNING, "xid=%u", xid);
-	GlobalCSN csn = GlobalCSNLogGetCSN(xid);
-
-	// elog(WARNING, "is dirty = %d page=%d count=%d",(int) *(slru->shared->page_dirty), 
-	// 	*(slru->shared->page_number), *(slru->shared->page_lru_count) );
-	PG_RETURN_UINT64(csn);
+	PG_RETURN_UINT64(xid);
 }
 
